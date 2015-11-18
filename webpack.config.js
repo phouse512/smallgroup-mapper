b6c1c8682508ee01b7db6734370569700e93fb0b
phouse512 <philiphouse2015@u.northwestern.edu>
2015-11-17 23:32:41 -0600

    working state

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@
 module.exports = {
   module: {
     loaders: [
-        {include: /\.json$/, loaders: ["json-loader"]},
-        {test: require.resolve("./lib/index"), loader: "expose?index" }
+        { include: /\.json$/, loaders: ["json-loader"]},
+        { test: require.resolve("./lib/index"), loader: "expose?index" }
+        // { test: /\.handlebars$/, loader: "handlebars-loader" }
     ]
   },
   resolve: {
